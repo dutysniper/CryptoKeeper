3922a50f254fbad24cd3efa45d13ba21e8b1822a
dutysniper <natarov.constantin@yandex.ru>
2025-07-15 12:25:59 +0200

    1st setup

--- CryptoKeeper/ViewController.swift
+++ CryptoKeeper/ViewController.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+	init() {
+		super.init(nibName: nil, bundle: nil)
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		view.backgroundColor = .yellow
+		
 	}
-
-
 }
 
